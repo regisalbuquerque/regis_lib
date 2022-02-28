a59ac44010a8159f7a0f8f1f41effb65b4340ac8
Regis Albuquerque <regis.albuquerque1@gmail.com>
2022-02-28 13:49:41 -0500

    Terminando o gerador

--- lib/generators/scaffold_templates/scaffold_templates_generator.rb
+++ lib/generators/scaffold_templates/scaffold_templates_generator.rb
@@ -1,14 +1,15 @@
-class ScaffoldTemplatesGenerator < Rails::Generators::NamedBase
+class ScaffoldTemplatesGenerator < Rails::Generators::Base
   source_root File.expand_path("templates", __dir__)
   
   def copy_template_file
-    copy_file "_form.html.erb.tt", "lib/templates/erb/scaffold/_form2.html.erb"
-   # copy_file "_partial_header.html.erb.tt", "app/views/#{file_name.pluralize}/_#{file_name}_header.html.erb"
-   # copy_file "_form.html.erb.tt", "app/views/#{file_name.pluralize}/_form.html.erb"
-   # copy_file "_new.html.erb.tt", "app/views/#{file_name.pluralize}/_new.html.erb"
-   # copy_file "edit.html.erb.tt", "app/views/#{file_name.pluralize}/edit.html.erb"
-   # copy_file "show.html.erb.tt", "app/views/#{file_name.pluralize}/show.html.erb"
+    copy_file "_form.html.erb.tt", "lib/templates/erb/scaffold/_form.html.erb.tt"
+    copy_file "edit.html.erb.tt", "lib/templates/erb/scaffold/edit.html.erb.tt"
+    copy_file "index.html.erb.tt", "lib/templates/erb/scaffold/index.html.erb.tt"
+    copy_file "new.html.erb.tt", "lib/templates/erb/scaffold/new.html.erb.tt"
+    copy_file "partial.html.erb.tt", "lib/templates/erb/scaffold/partial.html.erb.tt"
+    copy_file "show.html.erb.tt", "lib/templates/erb/scaffold/show.html.erb.tt"
     
-    copy_file "controller.rb.tt", "lib/templates/rails/controller2.rb"
+    copy_file "controller.rb.tt", "lib/templates/rails/controller.rb.tt"
+    copy_file "api_controller.rb.tt", "lib/templates/rails/api_controller.rb.tt"
   end
 end
