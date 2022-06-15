d184708871da48e38f1c2d0f83a94a322ffbe1cc
Regis Albuquerque <regis.albuquerque1@gmail.com>
2022-06-15 16:00:06 -0500

    Adicionando na lib

--- lib/regis_lib.rb
+++ lib/regis_lib.rb
@@ -2,6 +2,8 @@
 
 require_relative "regis_lib/version"
 require 'regis_lib/railtie' if defined?(Rails)
+require "regis_lib/app/models/concerns/filterable.rb"
+
 
 module RegisLib
   class Error < StandardError; end
