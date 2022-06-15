17fb39c0f129c5d1be78f1509696efcafceb15e9
Regis Albuquerque <regis.albuquerque1@gmail.com>
2022-06-15 18:05:22 -0500

    Adicionando o método de filtro em resultados

--- lib/app/models/concerns/filterable.rb
+++ lib/app/models/concerns/filterable.rb
@@ -9,6 +9,14 @@ module Filterable
       end
       results
     end
+    
+    def filter_from_results(results, filtering_params)
+      #results = self.where(nil)
+      filtering_params.each do |key, value|
+        results = results.public_send("filter_by_#{key}", value) if value.present?
+      end
+      results
+    end
   end
 
 end
